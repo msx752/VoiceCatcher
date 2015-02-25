67b588d746092d7672bcf08de284616ad8f96b24
Mustafa Salih Aslım <msalih@hotmail.co.uk>
2015-02-25 20:03:16 +0200

    Ses görseli güncellendi

--- VoiceCatcher1/Form1.Designer.cs
+++ VoiceCatcher1/Form1.Designer.cs
@@ -29,27 +29,51 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(329, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(15, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 60);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(368, 120);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- VoiceCatcher1/Form1.cs
+++ VoiceCatcher1/Form1.cs
@@ -14,24 +14,47 @@ namespace VoiceCatcher1
     public partial class Form1 : Form
     {
         AudioRecorder Mic = new AudioRecorder();
-        private double lastPeak;
+        float lastPeak;
+        Bitmap Empty = new Bitmap(300, 60);
+        int before = 0;
+        int step = 0;
 
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            pictureBox1.Image = Empty;
             Mic.MicrophoneLevel = 100;
-            Mic.SampleAggregator.MaximumCalculated += SampleAggregator_MaximumCalculated;
+            Mic.SampleAggregator.MaximumCalculated += new EventHandler<MaxSampleEventArgs>(SampleAggregator_MaximumCalculated);
             Mic.BeginMonitoring(0);
             Mic.BeginRecording(Application.StartupPath + "\\my1.wav");
         }
         private void SampleAggregator_MaximumCalculated(object sender, MaxSampleEventArgs e)
         {
-            lastPeak = Math.Floor(Math.Max(e.MaxSample, Math.Abs(e.MinSample)) * float.Parse("100"));
+            lastPeak = (int)Math.Floor(Math.Max(e.MaxSample, Math.Abs(e.MinSample)) * 100);
+            progressBar1.Value = (int)lastPeak;
             label1.Text = lastPeak.ToString();
+
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            if (step > 300)
+            {
+                gr.Clear(Color.White);
+                step = 0;
+            }
+            //Random rnd = new Random();
+            //lastPeak = rnd.Next(0, 101);
+            lastPeak = (lastPeak / 100) * pictureBox1.Height;
+            if (lastPeak == 0) lastPeak = 1;
+            float fark = (60 - lastPeak) / 2;
+            RectangleF wave = new RectangleF(new PointF(step, fark), new SizeF(1, lastPeak));
+            gr.FillRectangle(Brushes.Red, wave);
+            gr.Dispose();
+            pictureBox1.Refresh();
+            step++;
             //if (lastPeak > 1)
             //else
             //    label1.Text = "Komut Bekliyor..";
@@ -41,10 +64,13 @@ namespace VoiceCatcher1
             //else if (Mic.RecordingState == RecordingState.Stopped || Mic.RecordingState == RecordingState.RequestedStop)
             //    label1.Text = "Kayıt Tamamlandı..";
         }
+        int GetX(int x)
+        {
+            return (pictureBox1.Height - x) / 2;
+        }
 
         private void BeginRecording(string path)
         {
-            string waveFileName = path;
             Mic.BeginRecording(path);
         }
 
