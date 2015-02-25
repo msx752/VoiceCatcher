60a34485f6ca005313c54de578ce4b0ab21c7e73
Üsame Fethullah AVCI <takaharashujin@gmail.com>
2015-02-25 21:01:28 +0200

    Yeni Kontroller

--- VoiceCatcher1/Form1.cs
+++ VoiceCatcher1/Form1.cs
@@ -15,9 +15,13 @@ namespace VoiceCatcher1
     {
         AudioRecorder Mic = new AudioRecorder();
         float lastPeak;
+        string waveFileName = String.Empty;
+        int recStatus = 0;
+        bool logStatus = true, requestStatus = false;
         Bitmap Empty = new Bitmap(300, 60);
+        int before = 0;
         int step = 0;
-        int hassasiyet = 10;
+
         public Form1()
         {
             CheckForIllegalCrossThreadCalls = false;
@@ -30,19 +34,67 @@ namespace VoiceCatcher1
             Mic.MicrophoneLevel = 100;
             Mic.SampleAggregator.MaximumCalculated += new EventHandler<MaxSampleEventArgs>(SampleAggregator_MaximumCalculated);
             Mic.BeginMonitoring(0);
-            Mic.BeginRecording(Application.StartupPath + "\\my1.wav");
         }
-
-        bool KomutDinleniyor = false;
-        bool KomutAnlasildi = false;
-        DateTime ListenStart = new DateTime();
-        DateTime ListenStop = new DateTime();
         private void SampleAggregator_MaximumCalculated(object sender, MaxSampleEventArgs e)
         {
-            lastPeak = (int)Math.Floor(Math.Max(e.MaxSample, Math.Abs(e.MinSample)) * 100);
+            lastPeak = Math.Max(e.MaxSample, Math.Abs(e.MinSample)) * 100;
             progressBar1.Value = (int)lastPeak;
-            label1.Text = lastPeak.ToString();
+            DrawGraph();
 
+            if (requestStatus == false)
+            {
+                if (Mic.RecordingState == RecordingState.Monitoring)
+                {
+                    AddLog("Komut Bekleniyor!");
+                }
+                else if (Mic.RecordingState == RecordingState.Recording)
+                {
+                    AddLog("[" + Mic.RecordedTime + "]   komut kayıt ediliyor..");
+                }
+                else if (Mic.RecordingState == RecordingState.RequestedStop)
+                {
+                    logStatus = false;
+                    AddLog("İşlem kullanıcı tarafından durduruldu!");
+                }
+                else if (Mic.RecordingState == RecordingState.Stopped)
+                {
+                    //Buraya girdiğinde GoogleSpeechLib ile istek yollatacağız.
+                    //Gelen istek SampleAggregator_MaximumCalculated metodunda kontrol edilecek ve 
+                    //cevap geldiğinde işlemleri sıfırlayıp tekrar monitoring durumuna alacağız.
+                    logStatus = false;
+                    AddLog("İstek gönderiliyor..!");
+                }
+
+                if (lastPeak > 10)
+                    StartRecording();
+
+                // Burada konuşmanın kesildiği süreyi alıp işlem yapacak ve bitirecek
+                //Bittiğinde 59. satırdaki if'e girecek
+                int sure = 5;
+                if (sure > 10)
+                    StopRecording();
+
+                recLog.Items[recLog.Items.Count - 1].EnsureVisible();
+            }
+            else
+            {
+                AddLog("Cevap Bekleniyor..");
+            }
+        }
+
+        private void StopRecording()
+        {
+            Mic.Stop();
+        }
+
+        private void AddLog(string p)
+        {
+            if (logStatus == true)
+                recLog.Items.Add(p);
+        }
+
+        private void DrawGraph()
+        {
             Graphics gr = Graphics.FromImage(pictureBox1.Image);
             if (step > 300)
             {
@@ -57,48 +109,17 @@ namespace VoiceCatcher1
             gr.Dispose();
             pictureBox1.Refresh();
             step++;
-
-            if (lastPeak >= hassasiyet && !KomutDinleniyor)
-            {
-                ListenStart = DateTime.Now;
-                lblListen.Visible = true;
-                lblListenOk.Visible = false;
-                KomutDinleniyor = true;
-                KomutAnlasildi = false;
-                //KOMUT DİNLEME BURADA BAŞLATILACAK
-            }
-            else if (hassasiyet > lastPeak && KomutDinleniyor && !KomutAnlasildi)
-            {
-                ListenStop = DateTime.Now;
-                TimeSpan Sure = ListenStop - ListenStart;
-                if (Sure.TotalMilliseconds >= 500)
-                {
-                    lblListen.Visible = false;
-                    lblListenOk.Visible = true;
-                    KomutDinleniyor = false;
-                    KomutAnlasildi = true;
-                    //KOMUT DİNLEME BURADA SONLANDIRILACAK
-                }
-            }
-            else
-            {
-                DateTime IdleTime = DateTime.Now;
-                TimeSpan Sure = IdleTime - ListenStart;
-                if (Sure.TotalMilliseconds >= 2000)
-                {
-                    lblListen.Visible = false;
-                    lblListenOk.Visible = false;
-                    KomutDinleniyor = false;
-                    KomutAnlasildi = false;
-                    //DİNLEME İŞLEMİ YAPILMIYORKEN (MONİTORING AKTIFKEN) BURASI DEVREDE OLACAK HEP
-                }
-            }
         }
 
-
-        private void BeginRecording(string path)
+        int GetX(int x)
         {
-            Mic.BeginRecording(path);
+            return (pictureBox1.Height - x) / 2;
+        }
+
+        private void StartRecording()
+        {
+            waveFileName = Application.StartupPath + "\\files\\command_" + DateTime.Now.Millisecond + ".wav";
+            Mic.BeginRecording(waveFileName);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
