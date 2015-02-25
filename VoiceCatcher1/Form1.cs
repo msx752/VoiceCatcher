f7f935f151a756170030cb09f1353247e799c1fe
Mustafa Salih Aslım <msalih@hotmail.co.uk>
2015-02-25 02:27:20 +0200

    güncelleme
    kütüphaneyi projeye ekleme

--- VoiceCatcher1/Form1.cs
+++ VoiceCatcher1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VoiceRecorder.Audio;
 
 namespace VoiceCatcher1
 {
@@ -15,6 +16,11 @@ namespace VoiceCatcher1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
